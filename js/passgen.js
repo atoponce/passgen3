fa056a69611eb51f152fe16d043c69b50df3fc56
Aaron Toponce <aaron.toponce@gmail.com>
2023-04-19 11:55:25 -0600

    update jsdoc, add comments, remove unnecessary code, other improvements

--- js/passgen.js
+++ js/passgen.js
@@ -15,48 +15,56 @@ TEXTAREA.value = "Type here to generate your passwords.\n"
  * Initialize the Spritz state to a random state before keystrokes are entered.
  */
 function init() {
-    const aacs = new Uint32Array([0x09F91102, 0x9D74E35B, 0xD84156C5, 0x635688C0])
-    const fp = generateFingerprint()                        // generate basic browser fingerprint
-    const fpHash = SipHashDouble.hash_hex(aacs, fp)         // calculate 128-bit hash
-
     TEXTAREA.addEventListener("keydown", keyDown)
     TEXTAREA.addEventListener("keyup", keyUp)
 
-    if ("spritzState" in localStorage) {
-        Spritz.state = JSON.parse(localStorage.spritzState)
+    // If the Spritz state is saved from the last session and N didn't change
+    // size, load the state. Otherwise, initilaze Spritz with the new size of N.
+    if (window.localStorage.spritzState) {
+        const spritzState = window.localStorage.spritzState
+        if (N === JSON.parse(spritzState.length)) {
+            Spritz.S = JSON.parse(spritzState)
+        }
     }
 
-    // use current time (precision = milliseconds) as source randomness
-    let byteArr = timeToByteArray(Date.now())
-
-    absorb(byteArr)
-    absorbStop()
-
-    byteArr = []
-
-    for (let i = 0; i < fpHash.length; i += 2) {
-        let int = parseInt(fpHash.substring(i, i + 2), 16)
-        byteArr.push(int)
+    // Generate a unique browser fingerprint and convert to simple byte array.
+    const fpBytes = []
+    const fp = generateFingerprint()
+    
+    for (let i = 0; i < fp.length; i++) {
+        fpBytes.push(fp.charCodeAt(i))
     }
 
-    absorb(byteArr)
+    // Spritz is a capable hash function: hash(M, r) produces an r-byte hash of
+    // the input message (byte sequence) M defined as:
+    //      1. InitializeState()
+    //      2. absorb(M); absorbStop()
+    //      3. abosorb(r)
+    //      4. return squeeze(r)
+    // We're only interested in permuting the Spritz state, not returning an
+    // actual hash value. Thus we can skip steps 3 and 4.
+    absorb(fpBytes)
     absorbStop()
 
-    byteArr = timeToByteArray(Date.now())
-
-    absorb(byteArr)
-    absorbStop()
-
+    // Generate some random but difficult-to-type and generally long text for
+    // the user. From the Scripps Spelling Bee word list.
     randomScripps()
 }
 
 /**
  * Register a key press time in milliseconds and its value.
+ * Spritz can be used as a deterministic random bit generator (DRBG). As Spritz
+ * is a sponge construction, it is naturally a DRBG. The sponge state is the
+ * "entropy pool". New random input can be included at any time using aborb(I)
+ * and output may be extracted at any time using squeeze(r).
  * @param {Object} key - The keystroke
  * @returns true
  */
 function keyDown(key) {
-    const movementKeys = [" ", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", "PageDown", "PageUp", "Home", "End", "Tab"]
+    const movementKeys = [
+        " ", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown",
+        "PageDown", "PageUp", "Home", "End", "Tab"
+    ]
 
     if (movementKeys.includes(key.key)) {
         key.preventDefault()                // prevent keys from scrolling the page
@@ -90,7 +98,11 @@ function keyDown(key) {
 }
 
 /**
- * Register a key release time in milliseconds.
+ * Register a key release time in milliseconds and its value.
+ * Spritz can be used as a deterministic random bit generator (DRBG). As Spritz
+ * is a sponge construction, it is naturally a DRBG. The sponge state is the
+ * "entropy pool". New random input can be included at any time using aborb(I)
+ * and output may be extracted at any time using squeeze(r).
  * @param {Object} key - The keystroke
  * @returns true
  */
@@ -127,17 +139,26 @@ function timeToByteArray(time) {
  */
 function extract(r) {
     let q
-    const min = 256 % r
+    let min
+
+    r <= N ? min = N % r : min = r % N
 
     do {
-        q = squeeze(1) << 8 | squeeze(1)
+        q = squeeze(1) ** 2 + squeeze(1)
     } while (q < min)   // avoid biased choice
 
     return (q % r)
 }
 
 /**
- * Generate a character or word based on the extracted random number from Spritz.
+ * Generate a character or word based on the random number from Spritz.
+ * Each template is defined in the index.html for this function to follow when
+ * generating passwords. Given that we know the size of each word list and
+ * character set, we know how much entropy exists with each choice in the set.
+ * Key stroke entropy is collected in an "entropy pool". Depending on the
+ * template, we deduct the required entropy to generate each character/word in
+ * the password. As such, entropy is constantly increases and decreasing while
+ * keys are being typed and passwords are being generated.
  * @returns undefined
  */
 function addChar() {
@@ -161,15 +182,14 @@ function addChar() {
         return
     }
 
-    if (ENTROPY[0] === 0) {
-        ENTROPY[0] = 2 ** ENTROPYPERCHAR - 1
-    } else {
-        ENTROPY[0] = ENTROPY[0] << ENTROPYPERCHAR | 2 ** ENTROPYPERCHAR - 1
-    }
+    // Build up the "entropy pool"
+    ENTROPY[0] = ENTROPY[0] << ENTROPYPERCHAR | 2 ** ENTROPYPERCHAR - 1 // No parens, not a bug
 
+    // Find our template to follow generating the password
     TEXTAREA.scrollTop = TEXTAREA.scrollHeight
     tmplChar = TEMPLATE.value[NTMPL]
 
+    // Deduct entropy from the "entropy pool" as we build the password
     if (tmplChar === " ") {
         data = " "
         NTMPL++
@@ -201,11 +221,11 @@ function addChar() {
         if (ENTROPY[0] >= 2 ** 6 - 1) {
             let rand = extract(62)
             if (rand < 10) {
-                rand += 48                // 48 = '0'
+                rand += 48 // 48 = '0'
             } else if (rand < 36) {
-                rand += 55                // 55 + 10 = 'A'
+                rand += 55 // 55 + 10 = 'A'
             } else {
-                rand += 61                // 61 + 36 = 'a'
+                rand += 61 // 61 + 36 = 'a'
             }
             data = String.fromCharCode(rand)
             ENTROPY[0] >>= 6
@@ -221,9 +241,9 @@ function addChar() {
         if (ENTROPY[0] >= 2 ** 4 - 1) {
             let rand = extract(16)
             if (rand < 10) {
-                rand += 48                // 48 = '0'
+                rand += 48 // 48 = '0'
             } else {
-                rand += 55                // 55 + 10 = 'A'
+                rand += 55 // 55 + 10 = 'A'
             }
             data = String.fromCharCode(rand)
             ENTROPY[0] >>= 4
@@ -248,7 +268,7 @@ function addChar() {
 }
 
 /**
- * Clear the text area and reinitialize but carry over the existing Spritz state.
+ * Clear the text area and reinitialize but carry over the Spritz state.
  * @returns undefined
  */
 function clearPasswords() {
@@ -269,20 +289,27 @@ function clearPasswords() {
 
 /** Save the current Spritz state to disk.  */
 function saveEntropy() {
-    localStorage.setItem("spritzState", JSON.stringify(Spritz.state))
+    localStorage.setItem("spritzState", JSON.stringify(Spritz.S))
 }
 
-/** Generate some random text for the user to type from the Scripps Spelling Bee word list. */
+/** 
+ * Generate some random text for the user to type from the Scripps Spelling Bee
+ * word list. The words are generally long and dificult to prounounce, which
+ * should make them difficult to type as well. This improves entropy via
+ * irregular key stroke timings and typing mistakes.
+ */
 function randomScripps() {
     let rand
     const words = []
+    const len = scripps.length
+    const req = Math.ceil(256 / Math.floor(Math.log2(len)))
     
-    for (let i = 0; i < 26; i++) {
-        rand = extract(scripps.length)
+    for (let i = 0; i < req; i++) {
+        rand = extract(len)
         words.push(scripps[rand])
     }
 
-    const scrippsText = "Typing these words guarantee 256 bits security:\n\n"
+    const scrippsText = "Need something to type? These words provide 256-bits of entropy:\n\n"
     document.getElementById("scripps").value = scrippsText + words.join(" ")
 }
 
