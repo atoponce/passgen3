c8c4abe93d18fce876b40cfeedbd7a93fcf380b2
Aaron Toponce <aaron.toponce@gmail.com>
2023-04-23 11:15:18 -0600

    linting

--- js/fingerprint.js
+++ js/fingerprint.js
@@ -6,75 +6,79 @@
  * @returns {string} - A long text string of the data discovered.
  */
 function generateFingerprint() {
-    const fingerprint = []
+  const fingerprint = []
 
-    // User agent
-    fingerprint.push(window.navigator.userAgent)
+  // User agent
+  fingerprint.push(window.navigator.userAgent)
 
-    // Screen
-    fingerprint.push(window.screen.availWidth + "×" + window.screen.availHeight)
-    fingerprint.push(window.screen.width + "×" + window.screen.height)
+  // Screen
+  fingerprint.push(window.screen.availWidth + "×" + window.screen.availHeight)
+  fingerprint.push(window.screen.width + "×" + window.screen.height)
 
-    // Plugins
-    for (let i = 0; i < window.navigator.plugins.length; i++) {
-        fingerprint.push(window.navigator.plugins[i].name)
-    }
+  // Plugins
+  for (let i = 0; i < window.navigator.plugins.length; i++) {
+    fingerprint.push(window.navigator.plugins[i].name)
+  }
 
-    // Cookies
-    if (window.navigator.cookieEnabled) {
-        fingerprint.push(window.navigator.cookieEnabled)
-    } else {
-        document.cookie = "fp"
-        fingerprint.push(document.cookie.indexOf("fp") != -1)
-    }
+  // Cookies
+  if (window.navigator.cookieEnabled) {
+    fingerprint.push(window.navigator.cookieEnabled)
+  } else {
+    document.cookie = "fp"
+    fingerprint.push(document.cookie.indexOf("fp") != -1)
+  }
 
-    // localStorage
-    for (let i = 0; i < window.localStorage.length; i++) {
-        fingerprint.push(window.localStorage.key(i))
-        fingerprint.push(window.localStorage.getItem(window.localStorage.key(i)))
-    }
+  // localStorage
+  for (let i = 0; i < window.localStorage.length; i++) {
+    fingerprint.push(window.localStorage.key(i))
+    fingerprint.push(window.localStorage.getItem(window.localStorage.key(i)))
+  }
 
-    // sessionStorage
-    for (let i = 0; i < window.sessionStorage.length; i++) {
-        fingerprint.push(window.sessionStorage.key(i))
-        fingerprint.push(window.sessionStorage.getItem(window.sessionStorage.key(i)))
-    }
+  // sessionStorage
+  for (let i = 0; i < window.sessionStorage.length; i++) {
+    fingerprint.push(window.sessionStorage.key(i))
+    fingerprint.push(
+      window.sessionStorage.getItem(window.sessionStorage.key(i))
+    )
+  }
 
-    // Time zone
-    const date = new Date()
-    const offset = date.getTimezoneOffset() / 60
-    const name = date.toLocaleDateString(undefined, {day: '2-digit', timeZoneName: 'long'}).substring(4)
+  // Time zone
+  const date = new Date()
+  const offset = date.getTimezoneOffset() / 60
+  const name = date
+    .toLocaleDateString(undefined, { day: "2-digit", timeZoneName: "long" })
+    .substring(4)
 
-    if (offset > 0) {
-        fingerprint.push("GMT-" + ("0" + offset).slice(-2) + "/" + name)
-    } else {
-        fingerprint.push("GMT+" + ("0" + offset).slice(-2) + "/" + name)
-    }
+  if (offset > 0) {
+    fingerprint.push("GMT-" + ("0" + offset).slice(-2) + "/" + name)
+  } else {
+    fingerprint.push("GMT+" + ("0" + offset).slice(-2) + "/" + name)
+  }
 
-    // Language
-    fingerprint.push(window.navigator.language)
+  // Language
+  fingerprint.push(window.navigator.language)
 
-    // Mime types
-    for (let i = 0; i < window.navigator.mimeTypes.length; i++) {
-        fingerprint.push(window.navigator.mimeTypes[i].description)
-    }
+  // Mime types
+  for (let i = 0; i < window.navigator.mimeTypes.length; i++) {
+    fingerprint.push(window.navigator.mimeTypes[i].description)
+  }
 
-    // Canvas
-    const canvas = document.createElement("canvas")
-    const ctx = canvas.getContext("2d")
-    const txt = "https://github.com/atoponce/passgen3"
+  // Canvas
+  const canvas = document.createElement("canvas")
+  const ctx = canvas.getContext("2d")
+  const txt = "https://github.com/atoponce/passgen3"
 
-    ctx.textBaseline = "top"
-    ctx.font = "14px 'Arial'"
-    ctx.textBaseline = "alphabetic"
-    ctx.fillStyle = "#f60"
-    ctx.fillRect(125,1,62,20)
-    ctx.fillStyle = "#069"
-    ctx.fillText(txt, 2, 15)
-    ctx.fillStyle = "rgba(102, 204, 0, 0.7)"
-    ctx.fillText(txt, 4, 17)
+  ctx.textBaseline = "top"
+  ctx.font = "14px 'Arial'"
+  ctx.textBaseline = "alphabetic"
+  ctx.fillStyle = "#f60"
+  ctx.fillRect(125, 1, 62, 20)
+  ctx.fillStyle = "#069"
+  ctx.fillText(txt, 2, 15)
+  ctx.fillStyle = "rgba(102, 204, 0, 0.7)"
+  ctx.fillText(txt, 4, 17)
 
-    fingerprint.push(canvas.toDataURL())
+  fingerprint.push(canvas.toDataURL())
 
-    return fingerprint.join("|")
+  return fingerprint.join("|")
 }
